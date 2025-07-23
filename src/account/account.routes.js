8e19ffd21f672c595f7e3d6d70612ef5282b604c
chepe020 <joseajcu11@gmail.com>
2025-07-23 11:12:50 -0600

    error solucionado

--- src/account/account.routes.js
+++ src/account/account.routes.js
@@ -1,5 +1,5 @@
 import { getAccount, getAllAccounts } from "./account.controller.js";
-import { validatejwt } from "../middlewares/validate-JWT.js";
+import { validatejwt } from "../middlewares/validate-jwt.js";
 import { validateAdmin } from "../middlewares/validator-users.js";
 import { validateAccountQuery } from "../middlewares/validate-account-query.js";
 import { Router } from "express";
